c592596991749127a19ff25b89a6a556ef6d2909
Gaurav Kumar <53636036+gk9848970@users.noreply.github.com>
2024-01-18 18:01:00 +0530

    Create useBooleanHook.tsx

---
+++ useBooleanHook.tsx
@@ -0,0 +1,32 @@
+type UseBooleanOutput = {
+  value: boolean;
+  setValue: Dispatch<SetStateAction<boolean>>;
+  setFalse: () => void;
+  setTrue: () => void;
+  toggle: () => void;
+};
+
+function useBoolean(defaultValue?: boolean): UseBooleanOutput {
+  const [value, setValue] = useState(Boolean(defaultValue));
+
+  const setTrue = () => setValue(true);
+  const setFalse = () => setValue(false);
+  const toggle = () => setValue(!value);
+
+  return { value, setValue, setTrue, setFalse, toggle };
+}
+
+export default function Component() {
+  const { value, setValue, setTrue, setFalse, toggle } = useBoolean();
+
+  return (
+    <>
+      <p>
+        Value is <code>{value.toString()}</code>
+      </p>
+      <button onClick={setTrue}>set true</button>
+      <button onClick={setFalse}>set false</button>
+      <button onClick={toggle}>toggle</button>
+    </>
+  );
+}
